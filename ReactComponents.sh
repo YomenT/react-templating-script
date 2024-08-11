8d623563fa9d851009dc0b374466ebb937c1e6c6
YomenT <32947641+YomenT@users.noreply.github.com>
2024-08-11 18:36:42 +0400

    Update ReactComponents.sh

--- ReactComponents.sh
+++ ReactComponents.sh
@@ -3,7 +3,19 @@
 WATCH_DIR="/home/yomen/Documents/bluebird-teaching-frontend/src/components"
 CSS_DIR="/home/yomen/Documents/bluebird-teaching-frontend/src/css"
 
-COMPONENT_TEMPLATE='import React from "react";
+inotifywait -m -e create --format "%f" "$WATCH_DIR" | while read FILENAME; do
+    if [[ "$FILENAME" == *.js ]]; then
+        COMPONENT_NAME="${FILENAME%.js}"
+        CSS_FILE="$CSS_DIR/$COMPONENT_NAME.css"
+
+        FINAL_TEMPLATE="import React from \"react\";\n"
+
+        if [[ -f "$CSS_FILE" ]]; then
+            RELATIVE_CSS_PATH="../css/$COMPONENT_NAME.css"
+            FINAL_TEMPLATE="${FINAL_TEMPLATE}\nimport '$RELATIVE_CSS_PATH';"
+        fi
+
+        COMPONENT_TEMPLATE='
 
 const ComponentName = () => {
     return (
@@ -14,18 +26,6 @@ const ComponentName = () => {
 };
 
 export default ComponentName;'
-
-inotifywait -m -e create --format "%f" "$WATCH_DIR" | while read FILENAME; do
-    if [[ "$FILENAME" == *.js ]]; then
-        COMPONENT_NAME="${FILENAME%.js}"
-        CSS_FILE="$CSS_DIR/$COMPONENT_NAME.css"
-
-        FINAL_TEMPLATE="import React from \"react\";\n\n"
-
-        if [[ -f "$CSS_FILE" ]]; then
-            RELATIVE_CSS_PATH="../css/$COMPONENT_NAME.css"
-            FINAL_TEMPLATE="${FINAL_TEMPLATE}import '$RELATIVE_CSS_PATH';\n\n"
-        fi
 
         FINAL_TEMPLATE="${FINAL_TEMPLATE}${COMPONENT_TEMPLATE//ComponentName/$COMPONENT_NAME}"
 
